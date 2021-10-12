9b6ba4b7fefbf31e0eecfd07f9d5db350970aff5
eric.zheng <eric.zheng@cloudminds.com>
2021-10-12 10:23:11 +0800

    <backeard-cpp>: <add demo>
    1.first commit
    
    Bug-Id:
    Depends-On: N/A

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+//#include "backward-cpp/backward.hpp"
+
+//using namespace backward;
+
+
+void cal_test(void)
+{
+  int n =0;
+  int m = 10;
+  m = m/n;
+}
+int main(){
+  
+  cal_test();
+  
+}
